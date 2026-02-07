0ddf2b4b88b5515c4bc77c5b4c821b214b0c4ae5
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-02-07 10:40:05 +0530

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,23 @@
 import React, { useState } from "react";
+import Washroom from "./components/Washroom";
 
 const App = () => {
+  // const [num, setnum] = useState(0);
   // const arr = ["Chinmay", "Satyarth", "Shivam", "Rohit", "Satyarth"];
-  const [Marks, setMarks] = useState([90, 80, 70, 60, 28]);
+  // const [Marks, setMarks] = useState([90, 80, 70, 60, 28]);
+  const [Gender, setGender] = useState("Male")
 
-  function change() {
-    if (num === arr.length - 1) {
-      setnum(0);
-    } else {
-      setnum(num + 1);
-    }
+  function changeGender() {
+    if(Gender === "Male") return setGender("Female")
+      else if(Gender === "Female") return setGender("Other")
+    else return setGender("Male")
   }
 
-function giveGrace(){
-  const newMarks = Marks.map(function(elem){
-    if (elem <33){
-      return elem + 5;
-    }
-    else return elem;
-  })
-  setMarks(newMarks);
-}
-
-  const [num, setnum] = useState(0);
   return (
-    <div className="bg-black h-screen w-full flex flex-col justify-center items-center gap-5">
-      {Marks.map(function (elem, index) {
-        return (
-          <h1 className="text-3xl text-white font-semibold" key={index}>
-            Student {index + 1} got {elem} marks {elem <33? "[Fail]":"[Pass]"}
-          </h1>
-        );
-      })}
-      <button
-        onClick={giveGrace}
-        className="active:scale-95 bg-emerald-300 duration-200 ease-in px-5 py-3 mt-5 rounded-2xl font-medium text-1xl"
-      >
-        Give them Grace
-      </button>
-      {/* <h1 className=" font-bold underline text-9xl text-center text-blue-600">
-        {arr[num]}
-      </h1> */}
-      {/* <button onClick={change} className='bg-amber-300 duration-200 ease-in px-5 py-3 mt-5 rounded-2xl font-medium text-1xl'>Change</button> */}
+    <div className="bg-red-900 h-screen w-full flex flex-col justify-center items-center gap-5">
+      <h1 className="text-6xl text-white font-bold">Your Gender is ~{Gender}</h1>
+      <button onClick={changeGender} className="active:scale-90 bg-red-700 text-white px-4 py-2 text-1xl rounded-2xl font-medium">Change Gender</button>
+      <Washroom user = {Gender} />
     </div>
   );
 };
